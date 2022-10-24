e2e369cb4cc7d40e5e9646f22d8c59e9bc4ec590
userHWSEO <gmldnjseocnd@gmail.com>
2022-10-24 12:19:34 +0900

    docs: eslint, prettier 설정
    - 기본 eslint와 prettier 설정

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:page/recommended',
+    'prettier',
+  ],
+};
