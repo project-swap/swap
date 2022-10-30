4f48c35c318a63ff4d64ff9cba93a89c081c3b5c
HA-SEUNG-JEONG <gktmd652@naver.com>
2022-10-30 20:31:21 +0900

    Feat : 라우팅 기능 추가
    - React Query 기본세팅
    - Suspense 추가
    - Pages,Login,LogOut,SignUp,NotFound 페이지 추가
    - react-router-dom 설정

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Loader from './components/Loader';
+import Login from './pages/Login';
+import Mypage from './pages/Mypage';
+import NotFound from './pages/NotFound';
+import Pages from './pages/Pages';
+import SignUp from './pages/SignUp';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route path="/" element={<Pages />} />
+            <Route path="/mypage" element={<Mypage />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
