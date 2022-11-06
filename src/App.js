c0053ba691e16c2674613148ed9ca50d58619ffa
userHWSEO <gmldnjseocnd@gmail.com>
2022-11-06 16:41:35 +0900

    Revert "Merge branch 'dev' of https://github.com/project-swap/swap into dev"
    This reverts commit ff86eb53f8280f08898d78a5e809ecc16a4b173b, reversing
    changes made to 5539b0d704f85d072e29af591ab2b40a615a0056.

--- src/App.js
+++ src/App.js
@@ -1,31 +1,18 @@
-import React, { Suspense } from 'react';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Loader from './components/Loader';
-import Login from './pages/Login';
-import Mypage from './pages/Mypage';
-import NotFound from './pages/NotFound';
-import Pages from './pages/Pages';
-import SignUp from './pages/SignUp';
-
-const queryClient = new QueryClient();
+import React, { useEffect, useState } from 'react';
+import { firestore } from './firebase';
 
 function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Suspense fallback={<Loader />}>
-          <Routes>
-            <Route path="/" element={<Pages />} />
-            <Route path="/mypage" element={<Mypage />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Suspense>
-      </BrowserRouter>
-    </QueryClientProvider>
-  );
+  const [data, setData] = useState();
+  useEffect(() => {
+    const test = firestore.collection('test');
+    test
+      .doc('test_item')
+      .get()
+      .then(doc => {
+        setData(doc.data().food);
+      });
+  }, []);
+  return <div className="App">{data}</div>;
 }
 
 export default App;
