0dd012037e603664aec3131d2121a919758811b2
HA-SEUNG-JEONG <gktmd652@naver.com>
2022-11-06 21:01:51 +0900

    Style : MyPage 마크업
    - 프로필이미지 유저 정보 및 팔로잉, 위치설정,교환목록,북마크 마크업

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import React, { useEffect, useState } from 'react';
-import { firestore } from './firebase';
+import React from 'react';
+import Mypage from './pages/Mypage';
 
 function App() {
-  const [data, setData] = useState();
-  useEffect(() => {
-    const test = firestore.collection('test');
-    test
-      .doc('test_item')
-      .get()
-      .then(doc => {
-        setData(doc.data().food);
-      });
-  }, []);
-  return <div className="App">{data}</div>;
+  // const [data, setData] = useState();
+  // useEffect(() => {
+  //   const test = firestore.collection('test');
+  //   test
+  //     .doc('test_item')
+  //     .get()
+  //     .then(doc => {
+  //       setData(doc.data().food);
+  //     });
+  // }, []);
+  return (
+    <div className="App">
+      <Mypage />
+    </div>
+  );
 }
 
 export default App;
