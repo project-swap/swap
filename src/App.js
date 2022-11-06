cdab454cca51349deede3ab9fbde4ce46248f9e3
stardust6653 <stardust6653@gmail.com>
2022-11-07 01:12:29 +0900

    Feat: swap 페이지 및 register 페이지 라우터 추가

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ import Login from './pages/Login';
 import Mypage from './pages/Mypage';
 import NotFound from './pages/NotFound';
 import Pages from './pages/Pages';
+import RegisterProduct from './pages/RegisterProduct';
 import SignUp from './pages/SignUp';
+import Swap from './pages/Swap';
 
 const queryClient = new QueryClient();
 
@@ -20,6 +22,8 @@ function App() {
             <Route path="/mypage" element={<Mypage />} />
             <Route path="/signup" element={<SignUp />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/swap" element={<Swap />} />
+            <Route path="/register" element={<RegisterProduct />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </Suspense>
