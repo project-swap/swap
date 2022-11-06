d4f680dfecee637033703a9412f0cfb254eacbe3
stardust6653 <stardust6653@gmail.com>
2022-11-07 00:48:51 +0900

    제목은 대문자로 시작합니다. Style : 등록된 교환 게시글 목록 페이지 마크업 및 CSS 작업
    - 등록된 교환 게시글 목록 페이지 마크업

---
+++ src/components/Swap/ProductList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+import ProductCardList from './ProductCardList';
+import ProductListTap from './ProductListTap';
+
+const ProductListComponent = styled.div`
+  width: 80%;
+  padding: 2rem 5rem;
+  border: 1px solid black;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`;
+
+const ProductList = () => {
+  return (
+    <ProductListComponent>
+      <ProductListTap />
+      <ProductCardList />
+    </ProductListComponent>
+  );
+};
+
+export default ProductList;
