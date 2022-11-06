2088a2b937bc24700149dffc9129e0bb5fadc1e9
userHWSEO <gmldnjseocnd@gmail.com>
2022-11-06 16:58:50 +0900

    Revert "Revert "Merge branch 'dev' of https://github.com/project-swap/swap into dev""
    This reverts commit c0053ba691e16c2674613148ed9ca50d58619ffa.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { createGlobalStyle } from 'styled-components';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
 import reset from 'styled-reset';
-import Main from './pages/Main.js';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import Loader from './components/Loader';
+import Login from './pages/Login';
+import Mypage from './pages/Mypage';
+import NotFound from './pages/NotFound';
+import SignUp from './pages/SignUp';
+import Main from './pages/Main';
 
 const GlobalStyles = createGlobalStyle`
     ${reset}
 `;
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
     <div className="App">
+      <GlobalStyles />
       <BrowserRouter>
-        <GlobalStyles />
-        <Routes>
-          <Route path="/" element={<Main />}></Route>
-        </Routes>
+        <QueryClientProvider client={queryClient}>
+          <Suspense fallback={<Loader />}>
+            <Routes>
+              <Route path="/" element={<Main />} />
+              <Route path="/mypage" element={<Mypage />} />
+              <Route path="/signup" element={<SignUp />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Suspense>
+        </QueryClientProvider>
       </BrowserRouter>
     </div>
   );
