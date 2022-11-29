7a6061b6cf311480fe8ce106372e033751bd9e19
userHWSEO <gmldnjseocnd@gmail.com>
2022-11-29 21:50:34 +0900

    Style : 로딩 애니메이션 추가
    - 로딩 애니메이션 추가

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,7 +1,44 @@
 import React from 'react';
+import styled, { keyframes } from 'styled-components';
+
+const animation = keyframes`
+  0% {
+    top:0rem;
+  }
+  95% {
+    width: 15rem;
+  }
+  to {
+    top: 20rem;
+    width: 20rem;
+    height: 15rem;
+  } 
+`;
+
+const LodingAnimation = styled.div`
+  position: relative;
+  top: 0rem;
+  width: 15rem;
+  height: 15rem;
+  background-color: green;
+  border-radius: 50%;
+  animation: ${animation} 1s ease-in Infinite Alternate;
+`;
+
+const LodingContainer = styled.div`
+  display: flex;
+  width: 100%;
+  height: 50vw;
+  justify-content: center;
+  align-items: center;
+`;
 
 const Loader = () => {
-  return <div>Loading...</div>;
+  return (
+    <LodingContainer>
+      <LodingAnimation />
+    </LodingContainer>
+  );
 };
 
 export default Loader;
