cbcf48ddf7e59aa28c314eec285f3c1a64cb4606
userHWSEO <gmldnjseocnd@gmail.com>
2022-11-29 17:43:41 +0900

    Fix : 잘못된 Props 사용 수정
    - 잘못된 Props 사용법 수정

--- src/components/Main/SectionTitle.js
+++ src/components/Main/SectionTitle.js
@@ -6,43 +6,45 @@ import { Link } from 'react-router-dom';
 import { HiChevronRight } from 'react-icons/hi';
 import { linkStyle } from '../../styles/linkStyle';
 
-const SectionTitle = props => {
-  const { titleName, border, titleMargin } = props;
-
-  const Container = styled.section`
-    width: 23.13rem;
-  `;
-  const SectionName = styled.div`
-    height: 2.63rem;
-    color: #4a4a4a;
-    font-size: 1.25rem;
-    font-weight: bold;
-    margin-right: ${titleMargin
+const Container = styled.section`
+  width: 23.13rem;
+`;
+const SectionName = styled.div`
+  height: 2.63rem;
+  color: #4a4a4a;
+  font-size: 1.25rem;
+  font-weight: bold;
+  margin-right: ${props =>
+    props.titleMargin
       ? `
-    ${titleMargin}rem`
+    ${props.titleMargin}rem`
       : '7.5rem'};
-  `;
-  const TopContainer = styled.div`
-    display: flex;
-    justify-content: center;
-    border-bottom: ${border ? border : 'solid 3px black'};
-  `;
+`;
+const TopContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  border-bottom: ${props => (props.border ? props.border : 'solid 3px black')};
+`;
 
-  const ViewMoreContainer = styled.div`
-    display: flex;
-    align-items: center;
-    margin-bottom: 1rem;
-  `;
+const ViewMoreContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 1rem;
+`;
 
-  const ViewMore = styled.div`
-    font-size: 0.63rem;
-    margin-bottom: 0.1rem;
-  `;
+const ViewMore = styled.div`
+  font-size: 0.63rem;
+  margin-bottom: 0.1rem;
+`;
+
+const SectionTitle = props => {
   return (
     <>
       <Container>
-        <TopContainer>
-          <SectionName>{titleName}</SectionName>
+        <TopContainer border={props.border}>
+          <SectionName titleMargin={props.titleMargin}>
+            {props.titleName}
+          </SectionName>
           <ViewMoreContainer>
             <Link to="/" style={linkStyle}>
               <ViewMore>더보기</ViewMore>
