7829d5037b1239f6402ef266ddd068e98b3436dd
userHWSEO <gmldnjseocnd@gmail.com>
2022-11-29 15:34:30 +0900

    Add : Router.js 파일로 라우터 관리
    - 기존 App.js에서 관리하던 라우터들을 Router.js로 이동

--- src/App.js
+++ src/App.js
@@ -1,51 +1,10 @@
-import Mypage from './pages/Mypage';
-import React, { Suspense } from 'react';
-import { createGlobalStyle } from 'styled-components';
-import reset from 'styled-reset';
-import { RecoilRoot } from 'recoil';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import Loader from './components/Loader';
-import Login from './pages/Login';
-import NotFound from './pages/NotFound';
-import SignUp from './pages/SignUp';
-import Main from './pages/Main';
-
-const GlobalStyles = createGlobalStyle`
-    ${reset}
-`;
-
-const queryClient = new QueryClient();
+import React from 'react';
+import Router from '../src/pages/Router';
 
 function App() {
-  // const [data, setData] = useState();
-  // useEffect(() => {
-  //   const test = firestore.collection('test');
-  //   test
-  //     .doc('test_item')
-  //     .get()
-  //     .then(doc => {
-  //       setData(doc.data().food);
-  //     });
-  // }, []);
   return (
     <div className="App">
-      <GlobalStyles />
-      <BrowserRouter>
-        <QueryClientProvider client={queryClient}>
-          <RecoilRoot>
-            <Suspense fallback={<Loader />}>
-              <Routes>
-                <Route path="/" element={<Main />} />
-                <Route path="/mypage" element={<Mypage />} />
-                <Route path="/signup" element={<SignUp />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="*" element={<NotFound />} />
-              </Routes>
-            </Suspense>
-          </RecoilRoot>
-        </QueryClientProvider>
-      </BrowserRouter>
+      <Router />
     </div>
   );
 }
