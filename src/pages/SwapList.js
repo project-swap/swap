499daa3a3c5556b0e60f1055bff57435eb8dc7e9
HA-SEUNG-JEONG <gktmd652@naver.com>
2022-12-17 20:50:56 +0900

    Style : 교환 목록 페이지 스타일 수정

--- src/pages/SwapList.js
+++ src/pages/SwapList.js
@@ -3,9 +3,13 @@ import { Main } from './Mypage';
 import { Title } from './LocationSetting';
 import { FiCheckSquare } from 'react-icons/fi';
 import styled from 'styled-components';
+import SideBar from '../components/SideBar';
 
-const Container = styled.main`
+export const Container = styled.main`
   display: flex;
+  width: 120%;
+  position: relative;
+  right: 4rem;
   .check {
     flex-direction: column;
     padding: 3rem 1rem;
@@ -15,44 +19,49 @@ const Container = styled.main`
     width: 7rem;
     margin-top: 3rem;
   }
-  .list {
-    width: 10rem;
-  }
 `;
 
-const Select = styled.select`
-  width: 10rem;
+export const Select = styled.select`
+  width: 5rem;
+  padding-left: 1rem;
   height: 3rem;
   margin-top: 2rem;
   margin-right: 1rem;
 `;
 
-const Content = styled.h4`
+export const Content = styled.h4`
   margin: 12rem auto;
   opacity: 0.4;
 `;
 
 const SwapList = () => {
   return (
-    <Main>
-      <Container>
-        <Title className="list">교환목록</Title>
-        <FiCheckSquare className="check" />
-        <h4 className="title">내가 쓴 글</h4>
-        <FiCheckSquare className="check" />
-        <h4 className="title">최신순</h4>
-        <FiCheckSquare className="check" />
-        <h4 className="title">오래된순</h4>
-        <Select>
-          <option className="default" value="">
-            6개월
-          </option>
-          <option value="1">1</option>
-          <option value="2">2</option>
-        </Select>
-      </Container>
-      <Content>교환목록이 없습니다.</Content>
-    </Main>
+    <>
+      <SideBar />
+      <Main>
+        <Container>
+          <Title>교환목록</Title>
+          <FiCheckSquare className="check" />
+          <h4 className="title">내가 쓴 글</h4>
+          <FiCheckSquare className="check" />
+          <h4 className="title">최신순</h4>
+          <FiCheckSquare className="check" />
+          <h4 className="title">오래된순</h4>
+          <Select>
+            <option className="default" value="">
+              1개월
+            </option>
+            <option className="default" value="">
+              3개월
+            </option>
+            <option className="default" value="">
+              6개월
+            </option>
+          </Select>
+        </Container>
+        <Content>교환목록이 없습니다.</Content>
+      </Main>
+    </>
   );
 };
 
