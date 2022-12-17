faff06a586381203359eeb520b5ef1ea4fc361b0
HA-SEUNG-JEONG <gktmd652@naver.com>
2022-12-17 15:41:34 +0900

    Style : SideBar 스타일 수정 - linkStyle import 하여 기존 react-router-dom Link 스타일 변경

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import { linkStyle } from '../styles/linkStyle';
 
 const SideMain = styled.main`
   display: flex;
@@ -9,7 +11,7 @@ const SideMain = styled.main`
   height: 43rem;
   position: relative;
   top: 45rem;
-  left: 19.7rem;
+  right: -33rem;
   h2 {
     font-weight: 600;
     font-size: 1.5rem;
@@ -26,8 +28,12 @@ const SideMenu = styled.section`
   right: 10rem;
   line-height: 4rem;
   h3 {
-    opacity: 0.4;
     border-bottom: 1px solid;
+    font-weight: bold;
+    &:hover {
+      opacity: 0.5;
+      cursor: pointer;
+    }
   }
 `;
 
@@ -37,10 +43,18 @@ const SideBar = () => {
       <SideMain>
         <h2>마이페이지</h2>
         <SideMenu>
-          <h3>프로필</h3>
-          <h3>위치설정</h3>
-          <h3>교환목록</h3>
-          <h3>북마크</h3>
+          <Link to="/" style={linkStyle}>
+            <h3>프로필</h3>
+          </Link>
+          <Link to="/locationsetting" style={linkStyle}>
+            <h3>위치설정</h3>
+          </Link>
+          <Link to="/swaplist" style={linkStyle}>
+            <h3>교환목록</h3>
+          </Link>
+          <Link to="/bookmark" style={linkStyle}>
+            <h3>북마크</h3>
+          </Link>
           <h3>신고내역</h3>
         </SideMenu>
       </SideMain>
