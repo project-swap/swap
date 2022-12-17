3f1277bcef3d4d63b8056d8d95bd2d977cbb41f6
HA-SEUNG-JEONG <gktmd652@naver.com>
2022-12-17 15:30:27 +0900

    Feat : Mypage 마크업 - 하드코딩 방식을 map 함수를 통해 컴포넌트 렌더링 방식 변경

--- src/pages/Mypage.js
+++ src/pages/Mypage.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
 import profile from '../assets/logo/android-icon-144x144.png';
-import { AiOutlinePlusCircle } from 'react-icons/ai';
+import { AiFillEdit, AiOutlinePlusCircle } from 'react-icons/ai';
 import SideBar from '../components/SideBar';
 import NavBar from '../components/common/NavBar';
+import { FaTimes } from 'react-icons/fa';
 
 export const Main = styled.main`
   box-sizing: border-box;
@@ -48,7 +49,6 @@ const Location = styled.div`
 
 const LocationDataContainer = styled.div`
   display: flex;
-
   width: 97%;
   margin-bottom: 2rem;
 `;
@@ -59,39 +59,77 @@ const LocationData = styled.div`
   background-color: white;
   border: 1px solid black;
   margin-left: 1rem;
-  display: flex;
-  flex-direction: column;
+  h3 {
+    display: flex;
+    :nth-child(1) {
+      font-weight: bold;
+    }
+    margin-bottom: 0.5rem;
+    margin-top: 1rem;
+    margin-left: 0.5rem;
+  }
+
   &:nth-child(2),
   :last-child {
     justify-content: center;
     align-items: center;
   }
+  .icon {
+    display: flex;
+    justify-content: space-around;
+    cursor: pointer;
+    :nth-child(1),
+    :nth-child(2) {
+      :hover {
+        opacity: 0.5;
+      }
+    }
+  }
+  .plus {
+    display: flex;
+    margin: 3rem auto;
+    cursor: pointer;
+    :hover {
+      opacity: 0.5;
+    }
+  }
 `;
 
 const SwapContainer = styled.div`
-  width: 100%;
+  width: 97%;
   display: flex;
+  justify-content: space-around;
+  margin: 0 auto;
 `;
 
 const SwapAndBookMark = styled.div`
   display: flex;
-  width: 30%;
+  width: 33.3%;
   height: 18rem;
   background-color: white;
-  margin: 0 1rem;
+  padding: 0 2rem;
   border: 1px solid black;
   justify-content: space-between;
   h4 {
     font-weight: 600;
-    margin: 1rem 1rem;
+    margin: 1rem -1.5rem;
+    display: flex;
+    justify-content: space-between;
+    :hover {
+      :nth-child(2) {
+        cursor: pointer;
+        opacity: 0.5;
+      }
+    }
   }
 `;
 
 const MyPageMain = styled.div`
   display: flex;
   flex-direction: column;
-  position: relative;
-  bottom: 40rem;
+  /* position: relative;
+  bottom: 40rem; */
+  margin: -40rem auto;
   overflow: hidden;
 `;
 
@@ -114,23 +152,31 @@ const Mypage = () => {
             <h4>더보기</h4>
           </Location>
           <LocationDataContainer>
-            <LocationData></LocationData>
             <LocationData>
-              <AiOutlinePlusCircle />
+              <h3>자취방[main]</h3>
+              <h3>
+                서울특별시 강남구 논현동
+                <section className="icon">
+                  <AiFillEdit />
+                </section>
+                <section className="icon">
+                  <FaTimes />
+                </section>
+              </h3>
             </LocationData>
-            <LocationData>
-              <AiOutlinePlusCircle />
-            </LocationData>
+            {[...Array(2)].map(index => (
+              <LocationData key={index}>
+                <AiOutlinePlusCircle className="plus" size={30} />
+              </LocationData>
+            ))}
           </LocationDataContainer>
           <SwapContainer>
-            <SwapAndBookMark>
-              <h4>교환목록</h4>
-              <h4>더보기</h4>
-            </SwapAndBookMark>
-            <SwapAndBookMark>
-              <h4>북마크</h4>
-              <h4>더보기</h4>
-            </SwapAndBookMark>
+            {[...Array(2)].map(index => (
+              <SwapAndBookMark key={index}>
+                <h4>교환목록</h4>
+                <h4>더보기</h4>
+              </SwapAndBookMark>
+            ))}
           </SwapContainer>
         </Main>
       </MyPageMain>
