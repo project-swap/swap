1323b8639849e804c872576230e91736e9c5857f
stardust6653 <stardust6653@gmail.com>
2022-12-19 04:11:01 +0900

    feat : 포스팅 시간에 따라 출력되는 포스팅 시간이 달라지도록 기능 추가
    - 몇초 전, 1분 전, 몇분 전, 몇십분 전, 한시간 전, 두시간 전 으로 나눔

--- src/components/common/ProductCard/PostingTime.js
+++ src/components/common/ProductCard/PostingTime.js
@@ -11,8 +11,33 @@ StyledPostingTime.defaultProps = {
   marginRight: '0.5rem',
 };
 
-const PostingTime = ({ fontSize, marginRight }) => {
-  return <StyledPostingTime fontSize={fontSize} marginRight={marginRight} />;
+const PostingTime = ({ date, fontSize, marginRight }) => {
+  const krCurrentDate = new Date();
+  const postDate = new Date(date);
+
+  const minusDiff = (krCurrentDate.valueOf() - postDate.valueOf()) / 1000;
+
+  return (
+    <StyledPostingTime fontSize={fontSize} marginRight={marginRight}>
+      {(function () {
+        if (minusDiff < 10) {
+          return '몇초 전';
+        } else if (minusDiff > 10 && minusDiff < 60) {
+          return '1분 전';
+        } else if (minusDiff > 60 && minusDiff < 600) {
+          return '몇분 전';
+        } else if (minusDiff > 600 && minusDiff < 3600) {
+          return '몇십분 전';
+        } else if (minusDiff > 3600 && minusDiff < 7200) {
+          return '한시간 전';
+        } else if (minusDiff > 7200 && minusDiff < 10800) {
+          return '두시간 전';
+        } else {
+          return date;
+        }
+      })()}
+    </StyledPostingTime>
+  );
 };
 
 export default PostingTime;
