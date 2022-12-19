bcb2e3762adbe1bfd512beb81341ef1210077aa7
HA-SEUNG-JEONG <gktmd652@naver.com>
2022-12-19 10:49:44 +0900

    Style : SideBar 스타일 수정

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -9,9 +9,9 @@ const SideMain = styled.main`
   border: 1px solid black;
   width: 20rem;
   height: 43rem;
+  margin-left: 19.5rem;
   position: relative;
   top: 45rem;
-  right: -33rem;
   h2 {
     font-weight: 600;
     font-size: 1.5rem;
@@ -23,7 +23,7 @@ const SideMenu = styled.section`
   flex-direction: column;
   margin-top: 7rem;
   align-items: center;
-  position: relative;
+  position: absolute;
   width: 8rem;
   right: 10rem;
   line-height: 4rem;
@@ -39,26 +39,24 @@ const SideMenu = styled.section`
 
 const SideBar = () => {
   return (
-    <>
-      <SideMain>
-        <h2>마이페이지</h2>
-        <SideMenu>
-          <Link to="/" style={linkStyle}>
-            <h3>프로필</h3>
-          </Link>
-          <Link to="/locationsetting" style={linkStyle}>
-            <h3>위치설정</h3>
-          </Link>
-          <Link to="/swaplist" style={linkStyle}>
-            <h3>교환목록</h3>
-          </Link>
-          <Link to="/bookmark" style={linkStyle}>
-            <h3>북마크</h3>
-          </Link>
-          <h3>신고내역</h3>
-        </SideMenu>
-      </SideMain>
-    </>
+    <SideMain>
+      <h2>마이페이지</h2>
+      <SideMenu>
+        <Link to="/" style={linkStyle}>
+          <h3>프로필</h3>
+        </Link>
+        <Link to="/locationsetting" style={linkStyle}>
+          <h3>위치설정</h3>
+        </Link>
+        <Link to="/swaplist" style={linkStyle}>
+          <h3>교환목록</h3>
+        </Link>
+        <Link to="/bookmark" style={linkStyle}>
+          <h3>북마크</h3>
+        </Link>
+        <h3>신고내역</h3>
+      </SideMenu>
+    </SideMain>
   );
 };
 
