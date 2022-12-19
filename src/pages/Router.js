aca372914eec5a1446e4ae24dbfdde185a8c783d
HA-SEUNG-JEONG <gktmd652@naver.com>
2022-12-19 10:23:14 +0900

    feat : Router 설정 추가

--- src/pages/Router.js
+++ src/pages/Router.js
@@ -16,6 +16,9 @@ import Swap from '../pages/Swap';
 import Mypage from '../pages/Mypage';
 import Loader from '../pages/Loader';
 import UserChatting from '../pages/UserChatting';
+import LocationSetting from './LocationSetting';
+import BookMarkPage from './BookMarkPage';
+import SwapList from './SwapList';
 
 const GlobalStyles = createGlobalStyle`
     ${reset}
@@ -40,6 +43,9 @@ const Router = () => {
                 <Route path="/swap" element={<Swap />} />
                 <Route path="/register" element={<Register />} />
                 <Route path="/chat-user:id" element={<UserChatting />} />
+                <Route path="/locationsetting" element={<LocationSetting />} />
+                <Route path="/bookmark" element={<BookMarkPage />} />
+                <Route path="/swaplist" element={<SwapList />} />
                 <Route path="*" element={<NotFound />} />
               </Routes>
             </Suspense>
