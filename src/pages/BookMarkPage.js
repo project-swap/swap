e4cc1b6f64e59716d4bd37d266533beed0a64597
HA-SEUNG-JEONG <gktmd652@naver.com>
2022-12-19 15:04:36 +0900

    Style : 북마크 페이지 스타일 수정

--- src/pages/BookMarkPage.js
+++ src/pages/BookMarkPage.js
@@ -1,25 +1,46 @@
 import { FiCheckSquare } from 'react-icons/fi';
-import { Title } from './LocationSetting';
-import Main from './Main';
 import React from 'react';
 import ContentChildren from '../components/ContentChildren';
 import ContainerChildren from '../components/ContainerChildren';
+import MainContainer from '../components/common/MainContainer';
+import SideBar from '../components/SideBar';
+import styled from 'styled-components';
 
-const BookMarkPage = () => {
+const BookMarkContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: -40rem auto;
+`;
+
+const Title = styled.h3`
+  font-weight: 600;
+  margin: 2.5rem 5rem;
+  font-size: 1.5rem;
+  display: flex;
+  position: relative;
+  top: -3rem;
+`;
+
+const BookMark = () => {
   return (
-    <Main>
-      <ContainerChildren>
-        <Title>북마크</Title>
-        <FiCheckSquare className="check" />
-        <h4 className="title">내가 쓴 글</h4>
-        <FiCheckSquare className="check" />
-        <h4 className="title">최신순</h4>
-        <FiCheckSquare className="check" />
-        <h4 className="title">오래된순</h4>
-      </ContainerChildren>
-      <ContentChildren>교환목록이 없습니다.</ContentChildren>
-    </Main>
+    <BookMarkContainer>
+      <SideBar />
+      <MainContainer>
+        <ContainerChildren>
+          <Title>북마크</Title>
+          <FiCheckSquare className="check" />
+          <h4 className="title">최신순</h4>
+          <FiCheckSquare />
+          <h4 className="title">오래된순</h4>
+          <FiCheckSquare />
+          <h4 className="title">교환중</h4>
+          <FiCheckSquare />
+          <h4 className="title">교환 완료</h4>
+        </ContainerChildren>
+        <ContentChildren>교환목록이 없습니다.</ContentChildren>
+      </MainContainer>
+    </BookMarkContainer>
   );
 };
 
-export default BookMarkPage;
+export default BookMark;
