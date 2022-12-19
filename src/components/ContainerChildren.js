6b24689c525ffd57fa9c5bf7fa2ddce50917518d
HA-SEUNG-JEONG <gktmd652@naver.com>
2022-12-19 15:02:15 +0900

    Style : 스타일 수정

--- src/components/ContainerChildren.js
+++ src/components/ContainerChildren.js
@@ -3,18 +3,11 @@ import React from 'react';
 
 const Container = styled.main`
   display: flex;
-  width: 120%;
+  width: 100%;
   position: relative;
   right: 4rem;
-  .check {
-    flex-direction: column;
-    padding: 3rem 1rem;
-    position: relative;
-  }
-  .title {
-    width: 7rem;
-    margin-top: 3rem;
-  }
+  justify-content: space-evenly;
+  top: 3rem;
 `;
 
 const ContainerChildren = ({ children }) => {
