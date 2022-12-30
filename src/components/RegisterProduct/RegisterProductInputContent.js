2d2e8881abef16875939df4d1413d3193a018804
stardust6653 <stardust6653@gmail.com>
2022-12-30 14:32:53 +0900

    Styled : placeholder 및 padding 추가

--- src/components/RegisterProduct/RegisterProductInputContent.js
+++ src/components/RegisterProduct/RegisterProductInputContent.js
@@ -2,16 +2,17 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyledContentInput = styled.textarea`
-  width: 43.8rem;
-  height: 28rem;
+  width: 40rem;
+  height: 24rem;
   border: 1px solid black;
   border-radius: 0;
   margin-bottom: 1.5rem;
   resize: none;
+  padding: 2rem;
 `;
 
 const RegisterProductInputContent = () => {
-  return <StyledContentInput />;
+  return <StyledContentInput placeholder="글을 입력하세요!" />;
 };
 
 export default RegisterProductInputContent;
