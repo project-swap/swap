7babd33d9c0e4928d217de461fe382685ff32414
stardust6653 <stardust6653@gmail.com>
2023-01-04 13:14:15 +0900

    Style : 라디오 버튼 CSS 수정

--- src/components/RegisterProduct/RegisterProductTradeTypeGroup.js
+++ src/components/RegisterProduct/RegisterProductTradeTypeGroup.js
@@ -1,29 +1,58 @@
 import React from 'react';
 import styled from 'styled-components';
-import {
-  Button,
-  Label,
-  RegisterProductGroupComponent,
-} from '../common/PublicStyle';
+import { RegisterProductGroupComponent } from '../common/PublicStyle';
+
+const ButtonGroup = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  width: 40rem;
+`;
+
+const RadioBtn = styled.input`
+  color: black;
+  margin: 0 0.7rem;
+  margin-top: -0.1rem;
+  :checked {
+    background-color: rgb(25, 118, 248);
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    border: none;
+    width: 14px;
+    height: 14px;
+    border-radius: 100%;
+  }
+`;
+
+const Type = styled.span`
+  width: 4rem;
+`;
+
+const Label = styled.label`
+  margin-left: 0.1rem;
+  font-size: 1.1rem;
+  font-weight: 600;
+  text-align: center;
+  color: #333;
+  cursor: pointer;
+  :hover {
+    color: black;
+  }
+`;
 
 const RegisterProductTradeTypeGroups = () => {
-  const ButtonGroup = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: flex-start;
-    width: 40rem;
-  `;
-
   return (
     <RegisterProductGroupComponent>
-      <Label>거래종류</Label>
       <ButtonGroup>
-        <Button width="5rem" margin="0 0 0 1rem">
+        <Type>거래종류</Type>
+        <Label htmlFor="swap" class="type">
           교환
-        </Button>
-        <Button width="5rem" margin="0 0 0 1rem">
+        </Label>
+        <RadioBtn type="radio" name="type" id="swap" value="교환" check />
+        <Label htmlFor="share" class="type">
           나눔
-        </Button>
+        </Label>
+        <RadioBtn type="radio" name="type" id="share" value="나눔" />
       </ButtonGroup>
     </RegisterProductGroupComponent>
   );
