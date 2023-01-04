d288777d7ee8f18a47643b4dd051d9afeedaeeda
HA-SEUNG-JEONG <gktmd652@naver.com>
2023-01-04 20:45:48 +0900

    Feat : Test 컴포넌트 복구

--- src/pages/Router.js
+++ src/pages/Router.js
@@ -13,7 +13,7 @@ import Main from './Main';
 import Search from './Search';
 import Register from './Register';
 import Swap from './Swap';
-import Mypage from './mypage/Mypage';
+import Mypage from './Mypage';
 import Loader from './Loader';
 import UserChatting from './UserChatting';
 import LocationSetting from './LocationSetting';
@@ -21,6 +21,7 @@ import BookMarkPage from './BookMarkPage';
 import SwapList from './SwapList';
 import ReportHistory from './ReportHistory';
 import Profile from './Profile';
+import Test from './Test';
 
 const GlobalStyles = createGlobalStyle`
     ${reset}
@@ -50,6 +51,7 @@ const Router = () => {
                 <Route path="/swaplist" element={<SwapList />} />
                 <Route path="/report" element={<ReportHistory />} />
                 <Route path="/profile" element={<Profile />} />
+                <Route path="/test" element={<Test />} />
                 <Route path="*" element={<NotFound />} />
               </Routes>
             </Suspense>
