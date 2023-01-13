d40d89fd4a58d93bffdbbea8346d857b261e4b1f
userHWSEO <gmldnjseocnd@gmail.com>
2023-01-13 14:29:01 +0900

    Feat : 채팅 리스트 Dots 효과 기능 구현
    - Dots 클릭 시 슬라이드 효과
    - 다른 Dots 클릭 시 열려있던 슬라이드 접힘
    - 열려있는 Dots 재클릭 시 슬라이드 접힘

--- src/components/Chatting/ChattingList/ChatItem.tsx
+++ src/components/Chatting/ChattingList/ChatItem.tsx
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 import { BiDotsVerticalRounded } from 'react-icons/bi';
 
+interface IUserData {
+  id: number;
+  userName: string;
+  lastMessage: string;
+  notification: number;
+  active: boolean;
+}
+
 const ChattingItems = styled.ul``;
 const ChattingItem = styled.li`
+  position: relative;
   display: flex;
-  justify-content: space-between;
   align-items: center;
   width: 43.25rem;
   height: 2.8rem;
   border: solid 1px black;
   margin-bottom: 1rem;
+  gap: 1rem;
+  overflow: hidden;
 `;
+
 const UserName = styled.div`
   display: flex;
   justify-content: center;
@@ -26,31 +37,84 @@ const UserName = styled.div`
 `;
 const LastMessage = styled.div`
   display: flex;
-  justify-content: center;
+  position: relative;
+  justify-content: flex-start;
   align-items: center;
   width: 20rem;
   height: 2rem;
-  border: solid 1px black;
   cursor: pointer;
 `;
 const Notification = styled.div`
   display: flex;
+  position: absolute;
+  left: 39rem;
   justify-content: center;
   align-items: center;
-  width: 2rem;
-  height: 2rem;
+  width: 1.2rem;
+  height: 1.2rem;
   border: solid 1px black;
   border-radius: 50%;
+  font-size: 0.8rem;
+  background-color: orange;
 `;
 
 const NotNotification = styled.div`
-  width: 2rem;
-  height: 2rem;
+  display: flex;
+  position: absolute;
+  left: 39rem;
+  width: 1.2rem;
+  height: 1.2rem;
 `;
 
 const DotsVerticalIcon = styled(BiDotsVerticalRounded)`
   font-size: 2rem;
+  position: absolute;
   cursor: pointer;
+  left: 41rem;
+  z-index: 5;
+  &.active {
+    left: 32rem;
+    animation-duration: 0.4s;
+    animation-name: dotsSlideAnimation;
+    @keyframes dotsSlideAnimation {
+      from {
+        left: 40rem;
+      }
+      to {
+        left: 32rem;
+      }
+    }
+  }
+`;
+
+const SlideOptionsWrap = styled.div`
+  display: flex;
+  position: absolute;
+  cursor: pointer;
+  left: 34rem;
+  &.active {
+    left: 34rem;
+    animation-duration: 0.4s;
+    animation-name: openSlideAnimation;
+    @keyframes openSlideAnimation {
+      from {
+        left: 40rem;
+      }
+      to {
+        left: 34rem;
+      }
+    }
+  }
+`;
+const SlideOption = styled.div`
+  display: flex;
+  align-items: center;
+  height: 2.8rem;
+  padding: 0 0.5rem 0 0.5rem;
+  border-top: 1px solid black;
+  border-bottom: 1px solid black;
+  border-left: 1px solid black;
+  white-space: nowrap;
 `;
 
 const ChatItem = () => {
@@ -60,30 +124,47 @@ const ChatItem = () => {
       userName: '서희원',
       lastMessage: '뉴진스 하입보이요.',
       notification: 0,
+      active: false,
     },
     {
       id: 1,
       userName: '박소예',
       lastMessage: '반갑습니다.',
       notification: 0,
+      active: false,
     },
     {
       id: 2,
       userName: '정하승',
       lastMessage: '안녕하세요.',
       notification: 1,
+      active: false,
     },
     {
       id: 3,
       userName: '박민주',
       lastMessage: '감사합니다.',
       notification: 3,
+      active: false,
     },
   ];
+  const [userState, setUserState] = useState(mockData);
+  const copyUserState = [...userState];
+
+  const clickSlideOption = (el: IUserData) => {
+    if (!el.active) {
+      copyUserState.filter(el => {
+        el.active = false;
+      });
+    }
+    el.active = !el.active;
+    setUserState(copyUserState);
+  };
+
   return (
     <>
       <ChattingItems>
-        {mockData.map(el => {
+        {copyUserState.map(el => {
           return (
             <ChattingItem key={el.id}>
               <UserName>{el.userName}</UserName>
@@ -93,7 +174,22 @@ const ChatItem = () => {
               ) : (
                 <NotNotification />
               )}
-              <DotsVerticalIcon />
+              <DotsVerticalIcon
+                onClick={() => {
+                  clickSlideOption(el);
+                }}
+                className={el.active ? 'active' : 'inactive'}
+              />
+              {el.active ? (
+                <SlideOptionsWrap className={el.active ? 'active' : 'inactive'}>
+                  <SlideOption style={{ backgroundColor: 'red' }}>
+                    신고하기
+                  </SlideOption>
+                  <SlideOption style={{ backgroundColor: 'white' }}>
+                    나가기
+                  </SlideOption>
+                </SlideOptionsWrap>
+              ) : null}
             </ChattingItem>
           );
         })}
@@ -102,4 +198,4 @@ const ChatItem = () => {
   );
 };
 
-export default ChatItem;
+export default React.memo(ChatItem);
