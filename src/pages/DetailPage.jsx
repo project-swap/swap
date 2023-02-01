599d91df6cae53e7fd17e9db77a1cb2040271ca8
stardust6653 <stardust6653@gmail.com>
2023-02-02 00:21:55 +0900

    Style : 모달 => 페이지로 디자인 전환

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
-import BackgroundBlur from '../components/BackgroundBlur';
 import Footer from '../components/common/Footer';
 import NavBar from '../components/common/NavBar';
 import ProductDetail from '../components/ProductDetail/ProductDetail';
-import SalesProductList from '../components/SalesProduct/SalesProductList';
 
 const DetailPage = () => {
   return (
     <>
       <NavBar />
-      <SalesProductList />
-      <BackgroundBlur />
       <ProductDetail />
       <Footer />
     </>
