6ec2f75e2c234549104d5a2fdbf19dfe9aa7d6d6
stardust6653 <stardust6653@gmail.com>
2023-02-02 16:43:19 +0900

    feat : product card 의 content-preview overflow설정 및 말줌임표 설정

--- src/components/common/ProductCard/ArticlePreview.jsx
+++ src/components/common/ProductCard/ArticlePreview.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyledArticlePreview = styled.p`
+  white-space: normal;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
   margin: ${props => props.margin};
   padding-right: ${props => props.paddingRight};
   line-height: ${props => props.lineHeight};
