842b1db84f99f3de836b9293e448a0a916b8fd3d
stardust6653 <stardust6653@gmail.com>
2023-02-04 15:29:42 +0900

    feat : 최신 순 및 오래된 순 필터기능 추가

--- src/components/SalesProduct/SalesProductListTap.jsx
+++ src/components/SalesProduct/SalesProductListTap.jsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import { collection, getDocs, orderBy, query } from 'firebase/firestore';
+import React, { useState } from 'react';
 import { FiCheckSquare, FiSquare } from 'react-icons/fi';
+import { useSetRecoilState } from 'recoil';
 import styled from 'styled-components';
+import { db } from '../../firebase';
+import { filterData } from '../../atoms/atoms';
 
 const TapComponent = styled.div`
   display: flex;
@@ -40,22 +44,52 @@ const FilterName = styled.span`
 `;
 
 const SalesProductListTap = () => {
+  const [filterType, setFilterType] = useState('');
+  const setFilterDataArr = useSetRecoilState(filterData);
+
+  const filter = async type => {
+    const postRef = collection(db, 'posts');
+    const q = query(postRef, orderBy('convertDate', type));
+    const dataArr = [];
+    const querySnapShot = await getDocs(q);
+
+    querySnapShot.forEach(item => {
+      dataArr.push(item._document.data.value.mapValue.fields);
+    });
+
+    setFilterDataArr(dataArr);
+  };
+
+  const setMineFilter = async () => {
+    setFilterType('mine');
+  };
+
+  const setAscFilter = async () => {
+    setFilterType('asc');
+    filter('asc');
+  };
+
+  const setDescFilter = () => {
+    setFilterType('desc');
+    filter('desc');
+  };
+
   return (
     <TapComponent>
       <PageTitle>교환/나눔</PageTitle>
       <FilterGroup>
-        <Filter>
-          <FiCheckSquare />
+        <Filter onClick={setMineFilter}>
+          {filterType === 'mine' ? <FiCheckSquare /> : <FiSquare />}
           <FilterName>내가 쓴 글</FilterName>
         </Filter>
         <Bar />
-        <Filter>
-          <FiSquare />
+        <Filter onClick={setDescFilter}>
+          {filterType === 'desc' ? <FiCheckSquare /> : <FiSquare />}
           <FilterName>최신 순</FilterName>
         </Filter>
 
-        <Filter>
-          <FiCheckSquare />
+        <Filter onClick={setAscFilter}>
+          {filterType === 'asc' ? <FiCheckSquare /> : <FiSquare />}
           <FilterName>오래된 순</FilterName>
         </Filter>
       </FilterGroup>
