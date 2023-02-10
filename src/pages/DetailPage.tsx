c20440ff76953849313f8bb099eb4764b799cf94
stardust6653 <stardust6653@gmail.com>
2023-02-10 18:05:17 +0900

    fix : 폴더명 변경으로 발생한 경로에러 해결

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Footer from '../components/common/Footer';
 import NavBar from '../components/common/NavBar';
-import ProductDetail from '../components/productDetail/ProductDetail';
+import ProductDetail from '../components/product_detail/ProductDetail';
 
 const DetailPage = () => {
   return (
