bbfd7548a6a2211ce990fe7d327bc4323e42c767
HA-SEUNG-JEONG <gktmd652@naver.com>
2023-02-24 13:01:11 +0900

    Style : css 수정 및 Nav바 추가

--- src/pages/my_page/LocationSetting.tsx
+++ src/pages/my_page/LocationSetting.tsx
@@ -4,6 +4,7 @@ import { AiOutlinePlusCircle } from 'react-icons/ai';
 
 import SideBar from '../../components/SideBar';
 import MainContainer from '../../components/common/MainContainer';
+import NavBar from '../../components/common/NavBar';
 
 const LocationContainer = styled.div`
   display: flex;
@@ -13,10 +14,10 @@ const LocationContainer = styled.div`
 
 const Title = styled.h3`
   font-weight: 600;
-  margin: 2.5rem 5rem;
+  margin: 2.5rem 0;
   font-size: 1.5rem;
   display: flex;
-  margin-left: 2rem;
+  padding: 0 0 0 2rem;
 `;
 
 const CheckBox = styled.div`
@@ -48,7 +49,7 @@ const CheckBox = styled.div`
   }
   input {
     display: flex;
-    justify-content: start;
+    justify-content: center;
     margin: 1rem 0;
     position: relative;
     right: 13.5rem;
@@ -57,27 +58,36 @@ const CheckBox = styled.div`
   }
 `;
 
+const CheckBoxContainer = styled.div`
+  width: 50rem;
+  position: relative;
+  right: 5rem;
+`;
+
 const LocationSetting = () => {
   return (
-    <LocationContainer>
-      <SideBar />
-      <MainContainer>
-        <Title>위치설정</Title>
-        <div>
-          <CheckBox>
-            <input type="checkbox" />
-            <h3>자취방[main]</h3>
-            <h3>서울특별시 강남구 논현동</h3>
-          </CheckBox>
-          <CheckBox>
-            <AiOutlinePlusCircle className="icon" size={30} />
-          </CheckBox>
-          <CheckBox>
-            <AiOutlinePlusCircle className="icon" size={30} />
-          </CheckBox>
-        </div>
-      </MainContainer>
-    </LocationContainer>
+    <>
+      <NavBar />
+      <LocationContainer>
+        <SideBar />
+        <MainContainer>
+          <Title>위치설정</Title>
+          <CheckBoxContainer>
+            <CheckBox>
+              <input type="checkbox" />
+              <h3>자취방[main]</h3>
+              <h3>서울특별시 강남구 논현동</h3>
+            </CheckBox>
+            <CheckBox>
+              <AiOutlinePlusCircle className="icon" size={30} />
+            </CheckBox>
+            <CheckBox>
+              <AiOutlinePlusCircle className="icon" size={30} />
+            </CheckBox>
+          </CheckBoxContainer>
+        </MainContainer>
+      </LocationContainer>
+    </>
   );
 };
 
