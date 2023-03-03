42192c0ad2fc441ce69d87507cc32a5b316d2ff7
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2023-03-03 18:02:00 +0900

    Refactor : 컴포넌트 리팩토링 - 프리뷰 이미지에 대한 컴포넌트 파일 분리 - Form 안에 있던 컴포넌트들을 명시적으로 네이밍, props 전달 방식 사용 - 그로 인해 사용하지 않는 함수들 제거

--- src/components/ProfileImageModal.tsx
+++ src/components/ProfileImageModal.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { IoClose } from 'react-icons/io5';
 import BackgroundBlur from './BackgroundBlur';
@@ -13,26 +13,13 @@ import { useRecoilState } from 'recoil';
 
 import { ImgUrlArrState } from './../atoms/atoms';
 import { FileTypes } from './register_product/RegisterProductImageUploadGroup';
-import ImageInput from './my_page/ImageInput';
-
-const Form = styled.form`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
-
-const ProfileEdit = styled.span`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  font-weight: 600;
-  font-size: 1rem;
-`;
+import PreviewContainer from './my_page/PreviewContainer';
+import ImageUploader from './my_page/ImageUploader';
 
 const Modal = styled.section`
-  width: 28rem;
+  width: 30rem;
   height: 24rem;
+  display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
@@ -43,59 +30,24 @@ const Modal = styled.section`
   position: fixed;
 `;
 
-const ImageMessageContainer = styled.section`
-  width: 110%;
-  height: 5rem;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
-
-const ImageInputUpload = styled.input`
-  display: none;
-`;
-
-const ProfileWrap = styled.div`
-  width: 8rem;
-  height: 8rem;
-  margin: 0 auto;
-  position: relative;
-`;
-
-const PreviewImgPositionWrap = styled.div<{ url: string }>`
-  &::before {
-    content: '';
-    background-image: url(${props => props.url});
-    background-size: cover;
-    opacity: 0.5;
-    position: absolute;
-    inset: 0;
-  }
-`;
-
-const PreviewOuterImage = styled.div<{ url?: string }>`
-  width: 7.9rem;
-  height: 7.9rem;
-  background-image: url(${props => props.url});
-  background-size: cover;
-  background-position: center;
-  position: absolute;
-  border-radius: 4rem;
-  border: 1px solid black;
-`;
-
 const ModalContainer = styled.div`
   display: flex;
   justify-content: space-around;
   align-items: center;
-  margin: 2rem 0;
   .hover {
     cursor: pointer;
     &:hover {
       opacity: 0.3;
     }
   }
+`;
+
+const ProfileEdit = styled.span`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-weight: 600;
+  font-size: 1rem;
 `;
 
 const CloseButton = styled(IoClose)`
@@ -112,6 +64,13 @@ const Separator = styled.div`
   background-color: #000;
   display: block;
   margin: 1rem auto;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
 
 const ButtonContainer = styled.div`
@@ -136,32 +95,9 @@ interface CloseProps {
 const ProfileImageModal = ({ closeEvent }: CloseProps) => {
   const storage = getStorage();
   const [imgName, setImgName] = useState('');
-  const itemBeDragged = useRef(0);
-  const locationBeDragged = useRef(0);
 
   const [imgUpload, setImgUpload] = useState<FileTypes | null>(null);
   const [imgUrl, setImgUrl] = useRecoilState(ImgUrlArrState);
-
-  const startDrag = (index: number) => {
-    itemBeDragged.current = index;
-  };
-
-  const setDropLocation = (index: number) => {
-    locationBeDragged.current = index;
-  };
-
-  const handleDrop = () => {
-    const copyListItems = [...imgUrl];
-    const itemBeDraggedContent = copyListItems[itemBeDragged.current];
-
-    copyListItems.splice(itemBeDragged.current, 1);
-    copyListItems.splice(locationBeDragged.current, 0, itemBeDraggedContent);
-
-    itemBeDragged.current = 0;
-    locationBeDragged.current = 0;
-
-    setImgUrl(copyListItems);
-  };
 
   const uploadImageToStorage = () => {
     if (imgUpload === null) return;
@@ -180,36 +116,12 @@ const ProfileImageModal = ({ closeEvent }: CloseProps) => {
     });
   };
 
-  const handleImageUpload = (files: File) => {
-    setImgUpload(files);
-    setImgName(files.name);
-    const reader = new FileReader();
-    reader.onload = () => {
-      const id = Date.now() / files.name.length;
-      const imgUrlObj = {
-        url: URL.createObjectURL(files),
-        id: `images/${files.name}_${id}`,
-      };
-      setImgUrl(prevImgUrl => [...prevImgUrl, imgUrlObj]);
-    };
-    reader.readAsDataURL(files);
-  };
-
   const handleDeleteButtonClick = (event: React.MouseEvent<HTMLElement>) => {
     event.preventDefault();
     if (confirm('사진을 삭제하시겠습니까?')) {
       handleDelete(event);
       setImgUpload(null);
       setImgName('');
-    }
-  };
-
-  const handleImageInputChange = (
-    event: React.ChangeEvent<HTMLInputElement>,
-  ) => {
-    if (event.currentTarget.files !== null) {
-      const files = event.currentTarget.files[0];
-      handleImageUpload(files);
     }
   };
 
@@ -237,40 +149,19 @@ const ProfileImageModal = ({ closeEvent }: CloseProps) => {
           <CloseButton onClick={closeEvent} />
         </ModalContainer>
         <Separator />
-        <ProfileWrap>
-          {imgUrl.map((item, index) => {
-            return (
-              <PreviewImgPositionWrap
-                key={item.url}
-                url={item.url}
-                onDragOver={event => event.preventDefault()}
-                onDragStart={() => startDrag(index)}
-                onDragEnter={() => setDropLocation(index)}
-                onDragEnd={handleDrop}
-                draggable
-              >
-                <PreviewOuterImage url={item.url} />
-              </PreviewImgPositionWrap>
-            );
-          })}
-        </ProfileWrap>
+        <PreviewContainer imgUrl={imgUrl} />
         <Form onSubmit={handleSubmit}>
-          <ImageMessageContainer>
-            <ImageInput imgName={imgName} />
-            <ImageInputUpload
-              id="file-input"
-              type="file"
-              accept="image/*" //이미지 파일만 허용
-              required
-              onChange={handleImageInputChange}
-            />
-            <ButtonContainer>
-              <Button onClick={handleDeleteButtonClick} id={imgUrl[0]?.id}>
-                삭제
-              </Button>
-              <Button type="submit">저장</Button>
-            </ButtonContainer>
-          </ImageMessageContainer>
+          <ImageUploader
+            imgName={imgName}
+            setImgUpload={setImgUpload}
+            setImgName={setImgName}
+          />
+          <ButtonContainer>
+            <Button onClick={handleDeleteButtonClick} id={imgUrl[0]?.id}>
+              삭제
+            </Button>
+            <Button type="submit">저장</Button>
+          </ButtonContainer>
         </Form>
       </Modal>
       <BackgroundBlur />
